5aabf3d34db1df1691a577f9092bf77e5e525b70
Catherine Clause <cnclause123@gmail.com>
2019-08-21 03:01:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,7 +23,7 @@ def shift_with_args (array)
 end  
 
 def using_concat (array, array1)
-  my_favorite_things.concat(more_favs)
+  array.concat(more_favs)
 end  
 
 def using_insert (array, element)
