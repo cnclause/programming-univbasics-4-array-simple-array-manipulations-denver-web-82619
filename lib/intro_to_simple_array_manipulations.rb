45d62bf9ca975d2b7eb9d695616af1b6e76f29fd
Catherine Clause <cnclause123@gmail.com>
2019-08-21 02:28:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,9 @@ def using_push (array, element)
   array.push ("violet")
 end 
 
-def using_unshift (array,element)
+def using_unshift (array, element)
   array.unshift ("Staten Island")
 end 
 
-def 
+def using_pop (array, element)
+  
