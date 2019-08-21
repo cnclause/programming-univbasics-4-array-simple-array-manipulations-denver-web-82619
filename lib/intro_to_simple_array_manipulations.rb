e50739219f071f84f82feed9deb2259ca3513c13
Catherine Clause <cnclause123@gmail.com>
2019-08-21 02:47:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,7 +22,7 @@ def shift_with_args (array)
   array.shift(2)
 end  
 
-def using_concat (array, array)
+def using_concat (array, string)
   my_favorite_things.concat( ["sports cars", "flatiron school"])
 end  
 
